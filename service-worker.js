915c142302131f9415c81e1d4a62737e124cfcd7
giciek2137-collab <giciek2137@gmail.com>
2026-02-20 20:31:57 +0100

    Implement PWA display mode detection
    Add check for PWA display mode in service worker

--- service-worker.js
+++ service-worker.js
@@ -9,3 +9,9 @@ self.addEventListener("fetch", event => {
     fetch(event.request).catch(() => caches.match(event.request))
   );
 });
+
+if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true) {
+    // Aplikacja uruchomiona jako PWA (ekran główny)
+} else {
+    // Aplikacja uruchomiona w przeglądarce
+}
